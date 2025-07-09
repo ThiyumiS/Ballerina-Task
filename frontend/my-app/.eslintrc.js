e53cbed819e4e4f77acbf4fc2905093eed08a24c
Thiyumi Wickramasinghe <thiyumisasmitha@gmail.com>
2025-07-09 08:22:27 +0530

    configured ESLint

---
+++ frontend/my-app/.eslintrc.js
@@ -0,0 +1,10 @@
+thiyumi / Desktop / my / notBackup / Ballerina -
+  Task / frontend / my -
+  app / eslintrc.js;
+module.exports = {
+  extends: ["react-app", "plugin:cypress/recommended"],
+  plugins: ["cypress"],
+  env: {
+    "cypress/globals": true,
+  },
+};
